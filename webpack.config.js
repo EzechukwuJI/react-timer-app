934fda9478ec7075d91be5ae78e84994144cc7e6
ezechukwuji <ezechukwuji@gmail.com>
2018-06-22 08:49:05 +0100

    added support for jquery

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   entry: [
@@ -46,5 +47,10 @@ module.exports = {
       }
     ]
   },
+  // sassLoader: {
+  //   includePaths: [
+  //     path.resolve(__dirname, './node_modules/foundations-sites/scss')
+  //   ]
+  // },
    devtool: 'cheap-module-eval-source-map'
   };
